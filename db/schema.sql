68bfe7bb74bea46fa38a653792f6665d52aa7adc
Sowchirukuri <129994937+Sowchirukuri@users.noreply.github.com>
2023-07-31 08:14:20 -0500

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -2,25 +2,26 @@ DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 USE employee_db;
 
-CREATE TABLE departments (
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-department_name VARCHAR(255) NOT NULL
+
+CREATE TABLE department (
+  id INT PRIMARY KEY,
+  name VARCHAR(30)
 );
 
-CREATE TABLE roles (
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-title VARCHAR(255),
-salary DECIMAL(10,2),
-department_id INT,
-FOREIGN KEY (department_id)
-REFERENCES departments(id)
-ON DELETE SET NULL
+CREATE TABLE role (
+  id INT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department (id)
 );
 
 CREATE TABLE employee (
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-first_name VARCHAR(30) NOT NULL,
-last_name VARCHAR(30) NOT NULL,
-role_id INT,
-manager_id INT NOT NULL
+  id INT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES role (id),
+  FOREIGN KEY (manager_id) REFERENCES employee (id)
 );
